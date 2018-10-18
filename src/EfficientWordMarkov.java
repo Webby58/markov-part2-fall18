731734d8c5b7400d9ba47962d96fdc3efd6affe8
Webby58 <kabe.webster@duke.edu>
2018-10-17 23:23:31 -0400

    attempt again

--- src/EfficientWordMarkov.java
+++ src/EfficientWordMarkov.java
@@ -20,7 +20,32 @@ public class EfficientWordMarkov extends BaseWordMarkov{
 	@Override
 	public void setTraining(String text){
 		myWords = text.split("\\s+");
-		
+		myMap.clear();
+		 
+		for(int k=0; k <= myWords.length - myOrder; k += 1) { // goes through loop and makes sure that it goes through everything, being myOrder away from the end
+			WordGram currently = new WordGram(myWords, k, myOrder); // initialize WordGram, indexed with k
+			ArrayList<String> myList = new ArrayList<>(); // create empty array list
+			if(k + myOrder == myWords.length) {   // if length equals k + myOrder, at the end of the list
+				if(! myMap.containsKey(currently)) {
+					myList.add(PSEUDO_EOS);  // add pseudo because you are at the end of the list
+					myMap.put(currently, myList);
+					break; // code should stop because at end and should add pseudo
+				}
+				else {
+					myMap.get(currently).add(PSEUDO_EOS); // if duplicated, add to map again, code should break bc at end of list
+					break;
+				}
+			}
+			if(! myMap.containsKey(currently)) {
+				String nextWord = myWords[k+myOrder]; 
+				myList.add(nextWord);
+				myMap.put(currently, myList);
+			}
+			else { 
+				String nextWord = myWords[k + myOrder];
+				myMap.get(currently).add(nextWord);
+			}
+		}
 	}
 
 	@Override
